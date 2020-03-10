0d69fba4bdb537cda1b657ac59ce7e27533ef3f4
maki03117 <32335810+maki03117@users.noreply.github.com>
2020-03-10 14:05:23 +0000

    changes

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $message = '';
-  $connection = mysqli_connect("127.0.0.1", "maki", "team3", "product");
-
-  if (mysqli_connect_errno())
-    echo 'Failed to connect to the MySQL server: '. mysqli_connect_error();
-  else
-	$message = 'Connection is OK';
-?>
